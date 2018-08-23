f17684e3d9eada4cf2f96b740aae5dfdbe5b2950
Matteo Venditto <matteo.venditto@studio.unibo.it>
2018-08-23 15:30:28 +0200

    enhanced simulations web-view

--- visualization/app.py
+++ visualization/app.py
@@ -1,59 +1,99 @@
 from flask import Flask, send_file
 import os
 import datetime
+import itertools
+import time
 from sim_load import *
 from io import BytesIO
 
 app = Flask(__name__)
+app.config["CACHE_TYPE"] = "null"
 
-SIM_OUT_DIR = '/home/gazebo/output/'
+SIM_OUT_DIRS = [ ]
+OUT_DIR_MAP = dict()
 
 with open("root", "r") as root_dir:
-	SIM_OUT_DIR = root_dir.readline()
+	SIM_OUT_DIRS = list(filter(lambda x: not x[0].startswith('#'),[p.strip().split(':') for p in root_dir.readlines()]))
 
-print(" * {0}".format(SIM_OUT_DIR))
+for di in SIM_OUT_DIRS:
+	OUT_DIR_MAP[di[0].split('|')[0]] = di[1]
 
+print(SIM_OUT_DIRS)
+print(OUT_DIR_MAP)
+
+print(" * {0}".format(SIM_OUT_DIRS))
 
 def readable_date(ms):
 	return datetime.datetime.fromtimestamp(ms).strftime("%d/%m/%Y %H:%M:%S")
 
+def date_diff(d2, d1):
+        print(d1, d2)
+        d1 = datetime.datetime.strptime(d1, "%d/%m/%Y %H:%M:%S")
+        d2 = datetime.datetime.strptime(d2, "%d/%m/%Y %H:%M:%S")
+        return abs((d2 - d1).seconds) / 3600.
+
 def list_simulations():
-	return os.listdir(SIM_OUT_DIR)
+	return [(p, os.listdir(p[1])) for p in SIM_OUT_DIRS]
 
 def simulations_format():
 	sims = list_simulations()
 	content = ""
-	for s in sims:
-		try:
-			path = os.path.join(SIM_OUT_DIR, s)
-			start = readable_date(float(s))
-			ngens = get_gen_num(s)
-			row = """
-			<tr class="active">
-			   <td><a href="/sim/{0}">{0}</a></td>
-			   <td>{1}</td>
-			   <td>{2}</td>
-			   <td><button onclick="location.href='/sim/best_{0}'" class="btn btn-primary btn-sm"><i class="icon icon-download"></i></button></td>
-			</tr>
-			""".format(s, start, ngens)
-			content = content + row + "\n"
-		except:
-			continue
+	idx = 0
+	for sim in sims:
+		for s in sim[1]:
+			try:
+				rowc = "#e3e3e3" if idx % 2 == 0 else "#f0f1f4"
+				name = s
+				path = os.path.join(sim[0][1], name)
+				start = readable_date(float(name))
+				ngens, last_gen_t = get_gen_num(path)
+				last_update = date_diff(readable_date(time.time()), readable_date(last_gen_t))
+				loc,col = sim[0][0].split('|')
+				readme = os.path.join(path, 'README')
+				status, scol = ("check", "green") if os.path.exists(readme) else ("time", "gray")
+				end = "" if status == "time" else " - " + readable_date(os.stat(readme).st_mtime)
+
+				if status == "check":
+					end_time = readable_date(os.stat(readme).st_mtime)
+				if status == "time" and last_update > 5:
+					status,scol = ("cross", "red")
+
+				row = """
+				<tr class="active" style="background-color:{9}">
+				   <td><strong style="color:{4};">{3}<strong></td>
+				   <td><a href="/sim/{3}/{0}">{0}</a></td>
+				   <td>{1}</td>
+				   <td>{7}</td>
+				   <td><i class="icon icon-{5}" style="color:{8};"></i>{6}</td>
+				   <td>{2}</td>
+				   <td style="text-align:center;"><button onclick="location.href='/sim/{3}/best_{0}'" class="btn btn-primary btn-sm"><i class="icon icon-download"></i></button></td>
+				</tr>
+				""".format(name, start, ngens, loc, col, status, end, readable_date(last_gen_t), scol, rowc)
+				content = content + row + "\n"
+			except Exception as ex:
+				print(ex)
+				continue
+			idx += 1
 	return content
 
-def get_gen_num(sim_name):
-	generations = list(filter(lambda x: not x == 'README', os.listdir(os.path.join(SIM_OUT_DIR, sim_name))))
-	return len(generations)
+def get_gen_num(sim_path):
+	last_gen_n = get_last_gen(sim_path)
+	last_gen = os.path.join(sim_path, str(last_gen_n))
+	last_gen_time = os.stat(last_gen).st_mtime
+	# generations = list(filter(lambda x: not x == 'README', os.listdir(sim_path)))
+	#return len(generations)
+	return last_gen_n, last_gen_time
 
-@app.route('/sim/<sim_id>')
-def sim_plot(sim_id):
-	return plot_logbook(load_logbook(os.path.join(SIM_OUT_DIR, sim_id)))
+@app.route('/sim/<loc>/<sim_id>')
+def sim_plot(loc, sim_id):
+	return plot_logbook(load_logbook(os.path.join(OUT_DIR_MAP[loc], sim_id)))
 
-@app.route('/sim/best_<sim_id>')
-def sim_get_best(sim_id):
-	best =  get_best_indiv(os.path.join(SIM_OUT_DIR, sim_id))
+@app.route('/sim/<loc>/best_<sim_id>')
+def sim_get_best(loc, sim_id):
+	gen, best =  get_best_indiv(os.path.join(OUT_DIR_MAP[loc], sim_id))
+	print(gen, best)
 	return send_file(BytesIO(bytes(best, "utf-8")),
-		attachment_filename="best_{0}.py".format(sim_id.replace(".", "_")),
+		attachment_filename="best_{0}_{1}.py".format(sim_id.replace(".", "_"), gen),
 		as_attachment=True)
 
 @app.route('/sim')
@@ -76,8 +116,11 @@ def index():
 		<table class="table table-striped table-hover">
 		  <thead>
 		    <tr>
+		      <th>location</th>
 		      <th>name</th>
 		      <th>start</th>
+		      <th>last update</th>
+		      <th>status</th>
 		      <th>ngens</th>
 		      <th>best individual</th>
 		    </tr>
--- visualization/sim_load.py
+++ visualization/sim_load.py
@@ -7,6 +7,7 @@ from deap import base
 from deap import creator
 
 import pygal
+import shutil
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", array.array, typecode="f", fitness=creator.FitnessMax)
@@ -54,7 +55,7 @@ def get_best_indiv(sim_out_dir):
     with open(os.path.join(sim_out_dir, str(max_gen)), "rb") as gen_file:       
         p = pickle.load(gen_file)
         w = "weights = " + str(p["halloffame"].items[0]).replace("array('f', ", "").replace(")","") 
-        return w
+        return max_gen, w
 
     return "error!"
 
@@ -68,6 +69,12 @@ def load_logbook(sim_out_dir):
 
     return []
 
+MIN_VAL = -100.0
+
+def map_neg_infinity(values):
+    return map(lambda x: x if x != float("-inf") else MIN_VAL, values)
+
+
 def plot_logbook(logbook):
     y1 = list(map(lambda g: g['max'], logbook))
     y2 = list(map(lambda g: g['avg'], logbook))
@@ -78,9 +85,51 @@ def plot_logbook(logbook):
     line_chart = pygal.XY(show_dots=False)
     line_chart.title = 'Simulation Plot'
     
-    line_chart.add('max', list(zip(x,y1)))
-    line_chart.add('avg', zip(x,y2))
-    line_chart.add('min', zip(x,y3))
-    line_chart.add('std', zip(x,y4))
+
+    line_chart.add('max', list(zip(x, map_neg_infinity(y1))))
+    line_chart.add('avg', zip(x, map_neg_infinity(y2)))
+    line_chart.add('min', zip(x, map_neg_infinity(y3)))
+    line_chart.add('std', zip(x, map_neg_infinity(y4)))
     
     return PLOT_TEMPLATE.replace("{plot}", line_chart.render().decode("utf-8"))
+
+def term_plot_logbook(x, y):
+    tw,th = shutil.get_terminal_size()
+    print(tw,th)
+    x = x[0: tw - 2]
+    y = y[0: th - 2]
+
+    y = list(map(lambda yi: int(round(yi)),y))
+    rows = []
+    max_y = max(y)
+    histos = list(map(lambda j: ''.join(reversed((u'\u2588' * j) + (' ' * (max_y - j)))), y))
+
+    x_labels = list(map(str, x))
+    max_x_label_len = max(map(len, x_labels))
+
+    y_labels = list(map(str, y))
+    max_y_label_len = max(map(len, y_labels))
+
+    plot = ''
+    for i in range(0, max_y):
+        d = map(lambda h: h[i], histos)
+        plot += '\n'
+        y_l = str(max_y - i)
+        plot += y_l + (' ' * (max_y_label_len - len(y_l)))
+        plot += ''.join(d)
+        
+    plot += '\n'
+
+    for i in range(0, max_x_label_len):
+        labels_ = map(lambda l: l[i] if len(l) > i else ' ', x_labels)
+        plot += (' ' * max_y_label_len) + ''.join(labels_)
+        plot += '\n'
+
+    print(plot)
+
+if __name__ == '__main__':
+    outdir = "/home/mvend/Scrivania/simulations/output/1534262608.3884723/"
+    logbook = load_logbook(outdir)
+    x = list(map(lambda g: g['gen'], logbook))
+    y = list(map(lambda g: g['max'], logbook))
+    histos = term_plot_logbook(x,y)
